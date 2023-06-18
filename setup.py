5eca162958004ac2d5fc47d3286a0414609af793
ZHOU JESSE <zhou.jesse2@gmail.com>
2023-06-18 15:36:17 -0500

    test

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GNF Modules",
+    version="0.1",
+    packages=find_packages(),
+    url="https://github.com/JesseZhou-1/GNF-Modules",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
